5efe77e397d787844d0e3243bc77f6c55522578f
Inina125 <busmussina@gmail.com>
2023-01-11 16:08:38 +0200

    тема моб

--- src/js/theme-selector.js
+++ src/js/theme-selector.js
@@ -1,18 +1,25 @@
 function themeSelector() {
   const switchBtn = document.getElementById('switch');
+  const switchMobBtn = document.getElementById('switch-mob');
 
   let selectedTheme = localStorage.getItem('theme') || 'light';
   document.documentElement.classList.add(selectedTheme);
   if (selectedTheme === 'dark') {
     switchBtn.checked = true;
+    switchMobBtn.checked = true;
   }
 
   const handleClick = () => {
     let targetTheme = 'light';
+    let isChecked = false;
 
     if (selectedTheme === 'light') {
       targetTheme = 'dark';
+      isChecked = true;
     }
+
+    switchBtn.checked = isChecked;
+    switchMobBtn.checked = isChecked;
 
     document.documentElement.classList.remove(selectedTheme);
     document.documentElement.classList.add(targetTheme);
@@ -21,6 +28,7 @@ function themeSelector() {
   };
 
   switchBtn.addEventListener('click', handleClick);
+  switchMobBtn.addEventListener('click', handleClick);
 }
 
 export default themeSelector;
