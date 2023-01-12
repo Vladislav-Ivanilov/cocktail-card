8db8c71d011ec84fb3d16fb8cea37ece72d3fd32
Vladislav-Ivanilov <62110387+Vladislav-Ivanilov@users.noreply.github.com>
2023-01-12 13:09:31 +0200

    hero

--- src/js/hero/heroAPI.js
+++
@@ -1,7 +0,0 @@
-const URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php`;
-
-export function fetchCocktail(value) {
-  return fetch(`${URL}?f=${value}`).then(response => {
-    return response.json();
-  });
-}
